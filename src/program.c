6c07932b897a147d7c4502c5380499ac51a79f08
khetanishq <53037714+TanishqKhetan@users.noreply.github.com>
2020-10-02 17:16:43 +0530

    Update program.c

--- src/program.c
+++ src/program.c
@@ -1,12 +1,14 @@
+#define MAX 200
+#define MIN 21
 void setup(void)
 {
-	pinMode(21, OUTPUT);
+	pinMode(MIN, OUTPUT);
 }
 
 void loop(void)
 {
-	digitalWrite(21, HIGH);
-	delay(200);
-	digitalWrite(21, LOW);
-	delay(200);
+	digitalWrite(MIN, HIGH);
+	delay(MAX);
+	digitalWrite(MIN, LOW);
+	delay(MAX);
 }
